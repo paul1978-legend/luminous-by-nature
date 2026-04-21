d64330abff7dc3caa9b3efd3ea8cfcc9ad46784e
paul1978-legend <paulchamp@rocketmail.com>
2026-04-22 05:03:14 +1000

    fix casing + add classbento iframe

--- app/Workshops/page.tsx
+++ app/Workshops/page.tsx
@@ -1,16 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function Workshops() {
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://classbento.com.au/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <main className="bg-[#F5F1E8] text-[#1C1C1C] px-6 py-20 text-center">
 
@@ -20,7 +10,12 @@ export default function Workshops() {
         Join us for a hands-on stained glass experience. Browse upcoming workshops and book directly below.
       </p>
 
-      <div id="classbento-widget" className="max-w-5xl mx-auto"></div>
+      <div className="max-w-5xl mx-auto">
+        <iframe
+          src="https://classbento.com.au/9qcw2228p4au-widget?wtn=9qcw2228p4au&is_preview=1"
+          style={{ width: "100%", height: "1400px", border: "none" }}
+        />
+      </div>
 
     </main>
   );
